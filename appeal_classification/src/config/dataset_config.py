bb7b79599ac6d9c000b51ead77fa70d9d7b11854
h0tcl0g <daiki7069@gmail.com>
2025-01-13 18:24:03 +0900

    feat: RoBERTaよる訴求軸判定モデルを実装

---
+++ appeal_classification/src/config/dataset_config.py
@@ -0,0 +1,6 @@
+class DatasetConfig:
+    train_path: str = "hf://datasets/daiki7069/CAMERA-AA/train.csv"  # FIXME: 正しいデータセットのパスを設定する
+    # dev_path: str = "hf://datasets/daiki7069/CAMERA-AA/dev.csv"      # FIXME: 正しいデータセットのパスを設定する
+    dev_path: str = "hf://datasets/daiki7069/CAMERA-AA/train.csv"
+    # test_path: str = "hf://datasets/daiki7069/CAMERA-AA/test.csv"    # FIXME: 正しいデータセットのパスを設定する
+    test_path: str = "hf://datasets/daiki7069/CAMERA-AA/train.csv"
