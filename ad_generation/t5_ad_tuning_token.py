54ada02e19bb7ce6bb9f44c1cefcf77d89120314
h0tcl0g <daiki7069@gmail.com>
2025-01-18 17:49:43 +0900

    fix: ファクタリング

---
+++ ad_generation/t5_ad_tuning_token.py
@@ -0,0 +1,64 @@
+import argparse
+from src.tuning.dataset_handler import DatasetHandler
+from src.util.env import TEST_URL, DEV_URL, TRAIN_URL
+from src.util.env import PRETRAINED_MODEL_NAME
+from src.tuning.tsv_dataset_kw import TsvDataset
+from src.util.params import args_dict
+from src.util.env import MODEL_REPO_TOKEN
+from src.tuning.t5_fine_tuner import T5FineTuner
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from tqdm.auto import tqdm
+from transformers import (
+    AdamW,
+    T5ForConditionalGeneration,
+    T5Tokenizer,
+    get_linear_schedule_with_warmup
+)
+
+
+def main():
+    dataset = DatasetHandler(test_url=TEST_URL, dev_url=DEV_URL, train_url=TRAIN_URL)
+
+    dataset.csv2tsv()
+    
+    # トークナイザー（SentencePiece）モデルの読み込み
+    tokenizer = T5Tokenizer.from_pretrained(PRETRAINED_MODEL_NAME, is_fast=True)
+    
+    # 学習に用いるハイパーパラメータを設定する
+    args_dict.update({
+        "max_input_length":  512,  # 入力文の最大トークン数
+        "max_target_length": 64,  # 出力文の最大トークン数
+        "train_batch_size":  16,  # 訓練時のバッチサイズ
+        "eval_batch_size":   16,  # テスト時のバッチサイズ
+        "num_train_epochs":  8,  # 訓練するエポック数
+        })
+    args = argparse.Namespace(**args_dict)
+
+    train_params = dict(
+        accumulate_grad_batches=args.gradient_accumulation_steps,
+        gpus=args.n_gpu,
+        max_epochs=args.num_train_epochs,
+        precision= 16 if args.fp_16 else 32,
+        amp_level="O1",
+        amp_backend="apex",
+        gradient_clip_val=args.max_grad_norm,
+    )
+
+    # 転移学習の実行（GPUを利用すれば1エポック10分程度）
+    model = T5FineTuner(args)
+    trainer = pl.Trainer(**train_params)
+    trainer.fit(model)
+
+    # 最終エポックのモデルを保存
+    tokenizer.push_to_hub(MODEL_REPO_TOKEN)
+    model.push(MODEL_REPO_TOKEN)
+
+    del model
+
+    dataset.remove_tsv()
+
+
+if __name__ == "__main__":
+    main()
