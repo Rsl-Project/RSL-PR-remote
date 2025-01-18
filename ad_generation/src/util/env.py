5354ed0bd25cd57f1b72f8e7401e9115316c4545
h0tcl0g <daiki7069@gmail.com>
2025-01-18 17:51:45 +0900

    feat: 訴求軸なし生成

--- ad_generation/src/util/env.py
+++ ad_generation/src/util/env.py
@@ -2,7 +2,8 @@
 PRETRAINED_MODEL_NAME = "sonoisa/t5-base-japanese"
 
 # 転移学習後モデル
-MODEL_NAME = "daiki7069/t5_ad_kw_epoch8"
+MODEL_NAME = "daiki7069/t5_ad_kw_epoch20_wo-body"
+MODEL_REPO_TOKEN = "daiki7069/t5_ad_token"
 
 # データセット一時保存場所
 DATA_DIR = "/home/daiki_shibata/pj/ad-generation/RSL-PR-remote/ad_generation/__temp__"
@@ -13,4 +14,4 @@ DEV_URL = "hf://datasets/daiki7069/camera-minimal/dev.csv"
 TRAIN_URL = "hf://datasets/daiki7069/camera-minimal/train.csv"
 
 # モデル一時保存場所
-TEMP_MODEL_REPO = "daiki7069/temp_model"
+TEMP_MODEL_REPO = "daiki7069/t5_ad_kw"
